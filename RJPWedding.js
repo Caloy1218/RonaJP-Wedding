135d7e4ea297ba01a7c954c5645895fd5019cc7d
John Carlo Saquing <112403931+Caloy1218@users.noreply.github.com>
2023-01-04 13:53:33 +0800

    new

---
+++ RJPWedding.js
@@ -0,0 +1,30 @@
+let countDownDate = new Date("Jan 29, 2023 00:00:00").getTime();
+let x = setInterval(function () {
+  let dateNow = new Date().getTime();
+  let distance = countDownDate - dateNow;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.querySelector("#days").innerHTML = days;
+  document.querySelector("#hours").innerHTML = hours;
+  document.querySelector("#minutes").innerHTML = minutes;
+  document.querySelector("#seconds").innerHTML = seconds;
+}, 1000);
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
